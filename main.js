9a7942ba716bfa03171a75a8a31bbbf2db37ed63
AnhMinh <AnhMinh>
2023-09-14 22:43:26 +0700

    fix bug and add comment description for main.js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+/*
+Operand: phép toán
+currentOperand : phép toán hiện tại (số hiện tại)
+previousOperand : phép toán trước (số trước)
+
+hàm clear() gọi khi bấm nút AC -> xóa hết các phép toán
+
+hàm delete() gọi khi bấm nút DEL -> xóa 1 số hoặc 1 phép toán (+ - * /)
+
+appendNumber(number) hàm thêm số vào màn hình. gọi đến khi bấm vào các nút số
+nếu chưa tính xong thì có thể nhập thêm số đè vào 
+ví dụ: nhập 1, nhập tiếp 1 thì thành số 11
+nếu có dấu . tức là số phẩy thì trong 1 số chỉ được có 1 dấu .
+
+chooseOperation(operation) hàm chọn dấu phép tính. gọi đến khi bấm vào dấu
+nếu chưa có số thì không chọn được dấu
+nếu có số rồi mà vẫn bấm dấu thì thực hiện phép tính trước đó sau đó thực hiện đặt lại dấu 
+
+compute() hàm tính toán
+thực hiện tính toán và gán kết quả tính vào giá trị số hiện tại (currentOperand)
+
+getDisplayNumber(number) xử lý con số, thêm dấu phẩy nếu số dài
+
+updateDisplay() hiển thị số lên màn hình, được gọi mỗi khi thực hiện thao tác với máy tính
+*/
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
@@ -8,6 +33,7 @@ class Calculator {
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
+        this.doneCompute = true
         this.operation = undefined
     }
 
@@ -16,8 +42,13 @@ class Calculator {
     }
 
     appendNumber(number) {
+        if (this.doneCompute == true) {
+            this.currentOperand = ''
+            this.doneCompute = false
+        }
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
+
     }
 
     chooseOperation(operation) {
@@ -52,6 +83,7 @@ class Calculator {
                 return
         }
         this.currentOperand = computation
+        this.doneCompute = true
         this.operation = undefined
         this.previousOperand = ''
     }
@@ -78,7 +110,7 @@ class Calculator {
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
-            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.currentOperand)} ${this.operation}`
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         } else {
             this.previousOperandTextElement.innerText = ''
         }
@@ -93,6 +125,7 @@ const deleteButtons = document.querySelector('[data-delete]')
 const allClearButtons = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
